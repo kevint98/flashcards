d11b0a133fe53c497ae4aaeb9108de0b4e297b3c
Kevin Thompson <kevinthompson_@hotmail.com>
2022-01-19 03:09:05 +0000

    add new card and flip functionality

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import ROUTES from '../app/routes';
 import { selectTopics } from '../features/topics/topicsSlice';
 import { addQuizzForTopicId } from '../features/quizzes/quizzesSlice';
+import { addCard } from '../features/cards/cardsSlice';
 
 export default function NewQuizForm() {
 	const [name, setName] = useState('');
@@ -23,13 +24,19 @@ export default function NewQuizForm() {
 		const cardIds = [];
 
 		// create the new cards here and add each card's id to cardIds
+		cards.forEach(card => {
+			let cardId = uuidv4();
+			cardIds.push(cardId);
+			dispatch(addCard({ ...card, id: cardId }));
+		});
+
 		// create the new quiz here
 		dispatch(
 			addQuizzForTopicId({
 				id: uuidv4(),
 				name,
 				topicId,
-				cardIds: [],
+				cardIds: cardIds,
 			})
 		);
 
